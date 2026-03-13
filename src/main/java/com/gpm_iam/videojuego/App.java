42f3ee6e901c2722e9558af6f1e8e44b25c532de
Gary <garypaladines12@gmail.com>
2026-03-13 10:57:38 +0100

    Proyecto de Maven creado

---
+++ src/main/java/com/gpm_iam/videojuego/App.java
@@ -0,0 +1,13 @@
+package com.gpm_iam.videojuego;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
